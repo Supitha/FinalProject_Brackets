8dfe8187fbf941d6926a4ba8a7cb5e0fc5cfd220
Rajithkonara <rajithkonara@gmail.com>
2018-06-18 18:37:06 +0530

    list brokers

--- stockexchange/src/main/java/com/brackets/stockexchange/controller/BrokerController.java
+++ stockexchange/src/main/java/com/brackets/stockexchange/controller/BrokerController.java
@@ -1,9 +1,22 @@
 package com.brackets.stockexchange.controller;
 
+import com.brackets.stockexchange.model.Broker;
+import com.brackets.stockexchange.repository.BrokerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/broker")
 public class BrokerController {
+
+    @Autowired
+    private BrokerRepository brokerRepository;
+
+    @RequestMapping(path="/all", method = RequestMethod.GET)
+    public @ResponseBody Iterable<Broker> getAllBrokers() {
+        return brokerRepository.findAll();
+    }
 }
---
+++ stockexchange/src/main/java/com/brackets/stockexchange/repository/BrokerRepository.java
@@ -0,0 +1,10 @@
+package com.brackets.stockexchange.repository;
+
+import com.brackets.stockexchange.model.Broker;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by rajith on 6/18/18.
+ */
+public interface BrokerRepository extends CrudRepository<Broker, Integer>{
+}
