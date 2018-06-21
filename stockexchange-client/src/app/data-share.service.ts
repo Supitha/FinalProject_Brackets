7b69b1e13ea62c29a138c65fbdb5667bec001c76
Iroshan <iroshanav@gmail.com>
2018-06-21 22:38:20 +0530

    Data sharing services added

---
+++ stockexchange-client/src/app/data-share.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataShareService {
+
+private messageSource = new BehaviorSubject<string>("default message");
+currentMessage = this.messageSource.asObservable();
+
+  constructor() { }
+
+changeMessage(message:string){
+  this.messageSource.next(message)
+}
+  public static getUserName(){
+
+  }
+
+
+}
